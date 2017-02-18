3e13f1dc921c1cc0aada176c33b55ab7c755633d
Osadia <Osadia@S9S9S9S9S9>
2017-02-18 13:19:40 -0500

    Agregar archivos de proyecto.

---
+++ ShopingCar.Web/Models/DetallePedido.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ShopingCar.Web.Models
+{
+    public class DetallePedido
+    {
+        public int Id { get; set; }
+
+        #region Pedido
+        public int PedidoId { get; set; }
+        #endregion
+
+        #region Producto
+        public int ProductoId { get; set; }
+        #endregion
+    }
+}
---
+++ ShopingCar.Web/Models/Pedido.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Web;
+
+namespace ShopingCar.Web.Models
+{
+    public class Pedido
+    {
+        public int Id { get; set; }
+
+        #region cliente ID
+        public int ClienteId { get; set; }
+        #endregion
+
+        #region estado del pedido
+        public EstadoEstadoEnum EstadoPedido { get; set; }
+        #endregion
+
+        #region relacion con personas
+        public virtual ObservableCollection<Persona> Clientes { get; set; }
+        #endregion
+    }
+}
